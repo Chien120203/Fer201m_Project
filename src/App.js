b6371f653db528e9a8a83c465720d7365331c2bd
Chien120203 <chiendam120203@gmail.com>
2023-06-23 13:39:23 +0700

    update login signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Home from './components/Client/Home/Home';
-import ListProduct from './components/Client/ListProduct';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Client/Home/Home";
+import ListProduct from "./components/Client/ListProduct";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Client/Home/Home";
+import Login from "./components/common/Login/Login";
+import Signup from "./components/common/Register/Register";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/dien-thoai' element={<ListProduct />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dien-thoai" element={<ListProduct />} />
+
+        <Route path="/" element={<Home />} />
+        {/* <Route path='/about' element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
